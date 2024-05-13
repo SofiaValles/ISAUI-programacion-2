c9093be10d90ecbf453319367a144e492f292319
Sofía Valles <168451567+SofiaValles@users.noreply.github.com>
2024-05-13 17:21:47 -0300

    Update ejercicio 9  Calculadora de Factorial.py

--- ejercicio 9  Calculadora de Factorial.py
+++ ejercicio 9  Calculadora de Factorial.py
@@ -3,14 +3,19 @@
  #El factorial de un número entero positivo n se define como el producto de todos los enteros positivos menores o iguales a n. 
 #El programa debe manejar números enteros grandes y mostrar el resultado.
 
-numero = int (input("Ingrese un número entero positivo para calcular la factorial "))
-factorial = 0
-vueltas = 0
-acumulador = []
+numero = int(input("Ingrese un número entero positivo para calcular la factorial "))
+factorial = 1
 
-factorial= numero - vueltas
+if numero< 0:
+    print("Ingrese un número mayor a cero (positivo)")
+    
+if numero== 0 or numero== 1:
+    print("La factorial es : 1")
 
-while numero <= numero:
-    vueltas= vueltas +1
-    acumulador.append(numero)
+else:
+     for i in range(1, numero + 1):
+            factorial *= i
+     print("La factorial es: ", factorial)
+
+
     
