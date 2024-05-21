2e691345959c796db2cfa87631528325202a5e69
Sofía Valles <168451567+SofiaValles@users.noreply.github.com>
2024-05-21 17:30:33 -0300

    Add files via upload

---
+++ Guía_Funciones/Ejercicio_Funciones_1.py
@@ -0,0 +1,5 @@
+#Guía Ejercicios prácticos de Funciones
+#Ejercicio 1
+#Crea un función “ConvertirEspaciado”, que reciba como parámetro un texto y devuelve una
+#cadena con un espacio adicional tras cada letra. Por ejemplo, “Hola, tú” devolverá “H o l a , t ú “.
+# Crea un programa principal donde se use dicha función luego del ingreso del usuario.
