cdad9a25af3273a51ae039c7272903590bfcbbbc
Sofía Valles <168451567+SofiaValles@users.noreply.github.com>
2024-05-21 18:36:01 -0300

    Add files via upload

--- Guía_Funciones/Ejercicio_Funciones_1.py
+++ Guía_Funciones/Ejercicio_Funciones_1.py
@@ -3,3 +3,14 @@
 #Crea un función “ConvertirEspaciado”, que reciba como parámetro un texto y devuelve una
 #cadena con un espacio adicional tras cada letra. Por ejemplo, “Hola, tú” devolverá “H o l a , t ú “.
 # Crea un programa principal donde se use dicha función luego del ingreso del usuario.
+
+def ConvertirEspaciado(texto):
+    lista = ""
+    for letra in texto:
+        lista += letra + " "
+    return lista
+
+
+texto = input("Ingrese un texto: ")
+texto_con_espacios = ConvertirEspaciado(texto)
+print("Texto con espacios adicionales:", texto_con_espacios)
