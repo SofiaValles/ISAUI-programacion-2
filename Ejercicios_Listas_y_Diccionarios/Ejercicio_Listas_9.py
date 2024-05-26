9911e8f573287555736fd77e91a046fd3554561c
Sofía Valles <168451567+SofiaValles@users.noreply.github.com>
2024-05-26 19:57:23 -0300

    Ejercicio_Listas_9.py

--- Ejercicios_Listas_y_Diccionarios/Ejercicio_Listas_9.py
+++ Ejercicios_Listas_y_Diccionarios/Ejercicio_Listas_9.py
@@ -1,13 +1,13 @@
 #9. Producto de elementos: Escribe una función que tome una lista de números y
 #devuelva el producto de todos los elementos.
 def Producto_elementos(lista):
-    for i in range(lista):
-        
-        return producto
-
-
-
+    producto=1
+    for elemento in lista:    
+        producto= elemento * producto
+    return producto
 
 
 #prueba
 lista=[1,2,3,4,5]
+resultado= Producto_elementos(lista)
+print("su producto es: ", resultado)
