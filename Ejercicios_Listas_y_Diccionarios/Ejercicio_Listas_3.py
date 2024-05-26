81a28f62af46812fd544d7f2df8bc4f6cb303ba1
Sofía Valles <168451567+SofiaValles@users.noreply.github.com>
2024-05-26 19:15:15 -0300

    ejercicio calcular promedio de una lista

---
+++ Ejercicios_Listas_y_Diccionarios/Ejercicio_Listas_3.py
@@ -0,0 +1,15 @@
+#3. Promedio de una lista: Crea una función que calcule el promedio de los números en
+#una lista dada.
+
+def CalcularPromedio(lista):
+    promedio= sum(lista) /len(lista)
+    return promedio
+
+    #lo siguiente es para comprobar el funcionamiento de la función
+numeros=[]
+valores = input("ingrese nros")
+numeros.append(float(valores))
+
+#numeros = [1, 2, 3, 4, 5, 6]
+promedio = CalcularPromedio(numeros)
+print("El promedio de sus números es:", promedio)
