34b69d7208a5b4052446f6b333c8caee349ca135
Sofía Valles <168451567+SofiaValles@users.noreply.github.com>
2024-05-26 22:52:00 -0300

    ej filtrar diccionario

--- Ejercicios_Listas_y_Diccionarios/Ejercicio_Diccionarios_4.py
+++ Ejercicios_Listas_y_Diccionarios/Ejercicio_Diccionarios_4.py
@@ -1,18 +1,18 @@
 #4. Filtrar diccionario: Escribe una función que reciba un diccionario y una lista de claves,
 #y devuelva un nuevo diccionario solo con las claves especificadas.
 def Filtrar_Diccionario(dicc,lista):
-    
-
+    nuevo_dicc={}
+    for clave,valor in dicc.items():
+        if clave in lista:
+            nuevo_dicc[clave]= valor
     return nuevo_dicc
 
 #prueba
-dicc={
-    "a": 1,
+dicc={ "a": 1,
     "b": 2,
     "c": 3,
-    "d": 4
-}
-lista= {
-    "a": 1,
-    "c": 3
-}
+    "d": 4}
+lista= {"a": 1,
+    "c": 3}
+resultado= Filtrar_Diccionario(dicc,lista)
+print("Su nuevo dicc. con las claves especificadas es: ", resultado)
