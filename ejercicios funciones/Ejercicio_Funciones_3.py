3de01b52bdf621b0060753943c65199755c701b2
Sofía Valles <168451567+SofiaValles@users.noreply.github.com>
2024-05-27 12:02:44 -0300

    Delete ejercicios funciones directory

--- ejercicios funciones/Ejercicio_Funciones_3.py
+++
@@ -1,6 +0,0 @@
-#Ejercicio 3
-#Diseñar una función que calcule el área y el perímetro de una circunferencia. Utiliza dicha
-#función en un programa principal que lea el radio de una circunferencia y muestre su área y
-#perímetro.
-def Area_y_Perímetro():
-    
--- ejercicios funciones/Ejercicio_Funciones_7.py
+++
@@ -1,2 +0,0 @@
-#Contar letras: Crea una función que tome una cadena como entrada y devuelva un
-#diccionario con el recuento de cada letra en la cadena.
