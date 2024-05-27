abf3f47877a2426e481ed4fb6cf9fe24ae210d4b
Sofía Valles <168451567+SofiaValles@users.noreply.github.com>
2024-05-27 12:10:02 -0300

    Update Ejercicio_Listas_7.py

--- Ejercicios_Listas_y_Diccionarios/Ejercicio_Listas_7.py
+++ Ejercicios_Listas_y_Diccionarios/Ejercicio_Listas_7.py
@@ -1,15 +1,8 @@
 #7. Invertir lista: Escribe una función que tome una lista y devuelva una nueva lista con los
 #elementos en orden inverso.
-
 def Invertir_Lista(lista):
-    mayorindice=0
-    nuevalista=[]
-    for elemento in lista:
-        if elemento > mayorindice:
-            mayorindice=elemento
-            nuevalista.append(mayorindice)
-    return nuevalista
-
+    return lista[::-1]
+    
 #prueba
 lista= [1,6,2,3,4,5,9]
 alreves= Invertir_Lista(lista)
